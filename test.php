a7d75cc033d8a48a20fa3477065f759513ac2c18
Annabelle <annabellegamache@gmail.com>
2023-01-20 10:04:13 -0500

    add to Git

---
+++ test.php
@@ -0,0 +1,7 @@
+<h2>Variables POST</h2>
+<?php var_dump($_POST); ?>
+<?php echo "<pre>".print_r($_POST, true)."</pre>"; ?>
+
+<h2>Variables GET</h2>
+<?php var_dump($_GET); ?>
+<?php echo "<pre>".print_r($_GET, true)."</pre>"; ?>
